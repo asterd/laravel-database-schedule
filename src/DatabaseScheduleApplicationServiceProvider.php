d69ab258ebc965f4a57e2a5dbb8ab8db8d66fcda
ddurzo <dario.durzo@brunellocucinelli.it>
2025-08-08 10:23:49 +0200

    Fix paginator

--- src/DatabaseScheduleApplicationServiceProvider.php
+++ src/DatabaseScheduleApplicationServiceProvider.php
@@ -4,12 +4,15 @@ namespace RobersonFaria\DatabaseSchedule;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class DatabaseScheduleApplicationServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         $this->authorization();
+        // per Bootstrap 4.x (tu usi 4.6 / Cosmo)
+        Paginator::useBootstrapFour();
     }
 
     protected function authorization()
